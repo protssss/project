b996e574a65bcd46db7e7ee9bc4f4bcd27c7decf
Andrew <protsenko.andrew@gmail.com>
2022-08-20 19:12:36 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use stict";
+
+//alert ("hello");
+// const result = confirm ('Are u here ?');
+// console.log (result);
+
+
+// const answer = prompt('Вам есть 18 ?','18');
+// console.log(typeof(answer));
+
+const answers = [];
+answers[0] = prompt('What is your name ?','');
+answers[1] = prompt('How old are u ?','');
+answers[2] = prompt('What is your favotite color ?','Blue');
+document.write ('User name is ', answers[0]);
+document.write (' His age is ', answers[1]);
+document.write (' His favorite kolor is ', answers[2]);
